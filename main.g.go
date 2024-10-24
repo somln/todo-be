24885fdbeae95beb589bde5370fd042e488ce299
Somin Park <ps4708@naver.com>
2024-10-24 15:21:42 +0900

    Add CORS configuration

--- main.g.go
+++ main.g.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
@@ -13,6 +14,14 @@ func main() {
 	db.InitDB()
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
 	r.GET("/todos", handlers.GetTodos)
 	r.GET("/todos/:todoId", handlers.GetTodo)
